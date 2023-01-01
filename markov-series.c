b64cbb1408478e56864905cda2bfe96c9b17356d
Destructo <rich@cs.ucsb.edu>
2023-01-01 13:12:48 -0800

    dealing with negative numbers

--- markov-series.c
+++ markov-series.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #include "simple_input.h"
 #include "red_black.h"
@@ -16,11 +17,17 @@
  * including zeros, in each interval
  */
 
-char *Usage = "markov-series -f filename\n";
+char *Usage = "markov-series -f filename\n\
+\t-c sample_count\n\
+\t-i time interval (for synthetic trace)\n\
+\t-V <verbose mode>\n";
 
-#define ARGS "f:"
+#define ARGS "f:Vc:i:"
 
 char Fname[255];
+int Verbose;
+int Sample_count;
+double Interval;
 
 
 int main(int argc, char *argv[])
@@ -41,15 +48,31 @@ int main(int argc, char *argv[])
 	int src;
 	int dest;
 	double max;
+	double min;
 	double *counts;
+	double now;
+	double r;
+	double p;
+	double prev_p;
+	double start_ts;
 
 	memset(Fname,0,sizeof(Fname));
+	Interval = 1;
 	while((c = getopt(argc,argv,ARGS)) != EOF)
 	{
 		switch(c)
 		{
 			case 'f':
 				strncpy(Fname,optarg,sizeof(Fname));
+				break;
+			case 'V':
+				Verbose = 1;
+				break;
+			case 'c':
+				Sample_count = atoi(optarg);
+				break;
+			case 'i':
+				Interval = atof(optarg);
 				break;
 			default:
 				fprintf(stderr,
@@ -104,9 +127,18 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	max = -1;
+
+	max = -10000000.0;
+	min = 10000000.0;
 	state_count = 0;
+	start_ts = -1;
 	while(ReadData(input_data,field_count,values) != 0) {
+		/*
+		 * get starting ts for synth trace
+		 */
+		if(start_ts == -1) {
+			start_ts = values[0];
+		}
 		/*
 		 * assume value is in last field
 		 */
@@ -119,6 +151,10 @@ int main(int argc, char *argv[])
 			if(values[field_count-1] > max) {
 				max = values[field_count-1];
 			}
+			if(values[field_count-1] < min) {
+				min = values[field_count-1];
+			}
+	
 			hv.d = 1.0;
 			RBInsert(states,values[field_count-1],hv);
 		}
@@ -128,7 +164,7 @@ int main(int argc, char *argv[])
 	/*
 	 * cover all possible states
 	 */
-	state_count = (int)max + 1;
+	state_count = (int)(max - min) + 1;
 
 	transitions = (double *)malloc(state_count*state_count*sizeof(double));
 	if(transitions == NULL) {
@@ -140,37 +176,95 @@ int main(int argc, char *argv[])
 	memset(counts,0,state_count*sizeof(double));
 
 	Rewind(input_data);
-	src = -1;
+	/*
+	 * compute the transition counts and the count of each state
+	 * as a src
+	 */
+	src = -10000000;
 	while(ReadData(input_data,field_count,values) != 0) {
-		if(src == -1) {
-			src = (int)values[field_count-1];
+		if(src == -10000000) {
+			src = (int)(values[field_count-1] - min);
 			continue;
 		}
-		dest = (int)values[field_count-1];
+		dest = (int)(values[field_count-1]-min);
 		transitions[src*state_count+dest] += 1.0;
 		counts[src] += 1.0;
 		src = dest;
 	}
 
-	count = SizeOf(input_data);
-
-	printf("       ");
-	for(i=0; i < state_count; i++) {
-		printf("%6.6d ",i);
-	}
-	printf("\n");
-	for(i=0; i < state_count; i++) {
-		printf("--------");
-	}
-	printf("\n");
-	for(i=0; i < state_count; i++) {
-		printf("%4.4d | ",i);
-		for(j=0; j < state_count; j++) {
-//			printf("%4.4f ",transitions[i*state_count+j]/counts[i]);
-//			printf("%4.4f ",transitions[i*state_count+j]);
-			printf("%4.4f ",transitions[i*state_count+j]/(double)count);
+	if(Verbose == 1) {
+		count = SizeOf(input_data);
+		printf("       ");
+		for(i=0; i < state_count; i++) {
+			printf("%6.6d ",i+(int)min);
 		}
 		printf("\n");
+		for(i=0; i < state_count; i++) {
+			printf("--------");
+		}
+		printf("\n");
+		for(i=0; i < state_count; i++) {
+			printf("%4.4d | ",i+(int)min);
+			for(j=0; j < state_count; j++) {
+				printf("%4.4f ",transitions[i*state_count+j]/counts[i]);
+	//			printf("%4.4f ",transitions[i*state_count+j]);
+	//			printf("%4.4f ",transitions[i*state_count+j]/(double)count);
+			}
+			printf("\n");
+		}
+	}
+
+	if(Sample_count == 0) {
+		Sample_count = SizeOf(input_data);
+	}
+	/*
+	 * generate a synthetic series using the conditional transition
+	 * probabilities
+	 */
+	src=0;
+	now = start_ts;
+	for(i=0; i < Sample_count; i++) {
+		r = drand48();
+		/*
+		 * find the dest state
+		 */
+		dest = -1;
+		p = 0;
+		for(j=0; j < state_count; j++) {
+			prev_p = p;
+			p = (transitions[src*state_count+j] / counts[src]) + p;
+			if(r <= p) {
+				dest = j;
+				break;
+			}
+/*
+			if(j != 0) {
+				prev_p = transitions[src*state_count+(j-1)] / counts[src];
+			}
+			if((j == 0) && (r <= p)) {
+				dest = j;
+				break;
+			}
+*/
+/*
+			} else if((j != 0) && (r > prev_p) && (r <= p)) {
+				dest = j;
+				break;
+			} else if((j == (state_count-1)) && (r > p)) {
+				dest = j;
+				break;
+			}
+*/
+		}
+		// sanity check
+		if(dest == -1) {
+			fprintf(stderr,
+			"error: i: %d, src: %d, dest -1\n",i,src);
+			exit(1);
+		}
+		printf("%10.0f %d\n",now,dest+(int)min);
+		src=dest;
+		now += Interval;
 	}
 
 		
